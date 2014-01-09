1215c53cef6800da745f897f52632a201b41cb60
patorash <chariderpato@gmail.com>
2014-01-09 21:40:17 +0900

    scssを読み込めるようにした。あとangular.jsを読み込むようにもした。

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'slim'
 require 'coffee-script'
+require 'sass'
 
 class CoffeeRoute < Sinatra::Base
   get '/coffee/application' do
@@ -9,9 +10,16 @@ class CoffeeRoute < Sinatra::Base
   end
 end
 
+class ScssRoute < Sinatra::Base
+  get '/scss/application' do
+    scss :'scss/application'
+  end
+end
+
 
 class Pheasant < Sinatra::Base
   use CoffeeRoute
+  use ScssRoute
   get '/' do
     slim :index
   end
