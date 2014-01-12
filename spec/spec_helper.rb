0675fac4a100d6c9702634c3d112eedf0027f701
mkonishi <konishi@office-ore.net>
2014-01-12 19:39:58 +0900

    rake db:migrate 出来るようにした

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,11 @@
-require File.join(File.dirname(__FILE__), '..', 'app.rb')
 
 require 'sinatra'
 require 'rack/test'
 require 'rspec'
 
 set :environment, :test
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+
 set :run, false
 set :raise_errors, true
 set :logging, false
